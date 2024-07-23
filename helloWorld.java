2c3377f8901cefa96fb086940cf18d071848e099
httpjaved <javed.ahmed@akasaair.com>
2024-07-23 23:36:55 +0530

    primitive data types

--- helloWorld.java
+++ helloWorld.java
@@ -1,10 +1,17 @@
 class helloWorld{
 
     public static void main(String args[]){
-        int num1 = 10;
-        int num2 = 20;
-        int result = num1 + num2;
-        System.out.println(result);
+     int num1 = 20;
+     short sh = 558;
+     long l = 56756l;
+     byte b = 127;
+
+     float f = 5.6f;
+     double d = 3.4;
+
+       char c = 'J';
+
+     boolean bb = true;
 
     }
 }
