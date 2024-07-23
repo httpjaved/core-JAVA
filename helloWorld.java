a496455655a5423b6efaf7d850a5523e069163e9
httpjaved <javed.ahmed@akasaair.com>
2024-07-23 23:12:21 +0530

    variables

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,10 @@
 class helloWorld{
 
     public static void main(String args[]){
-        System.out.println("hello world");
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1 + num2;
+        System.out.println(result);
 
     }
 }
